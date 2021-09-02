e47b2b1928b0e1d6fb2876f899e06a812b103680
Jim Hessin <jhessin@gmail.com>
2021-09-02 12:24:48 -0600

    added electron target

---
+++ electron/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.grillbrickstudios.rev',
+  appName: 'REV App - Offline',
+  webDir: 'www',
+  bundledWebRuntime: true,
+};
+
+export default config;
