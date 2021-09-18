a87358b6fb20fe19b109e05194d144c19d7ac2b8
Jim Hessin <jhessin@gmail.com>
2021-09-17 19:11:36 -0600

    fixed google font imports
    I Hope?

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,72 +1,86 @@
 import { Font } from './models';
 
 export const Fonts: Font[] = [
+  // Merriweather
   {
+    label: 'Merriweather',
     heading: 'Default Serif Fonts',
     headingStyle: 'Merriweather, "Times New Roman", serif',
     value: 'Merriweather, "Times New Roman", serif',
-    label: 'Merriweather',
   },
   {
-    value: '"Times New Roman", Times, serif',
     label: 'Times New Roman',
+    value: '"Times New Roman", Times, serif',
   },
+  // Caladea
   {
-    value: 'Caladea, "Times New Roman", Serif',
     label: 'Caladea',
+    value: 'Caladea, "Times New Roman", Serif',
   },
+  // IBM Plex Serif
   {
-    value: '"ibm plex serif", "Times New Roman", Serif',
     label: 'IBM Plex Serif',
+    value: '"ibm plex serif", "Times New Roman", Serif',
   },
   {
+    label: 'Arial',
     heading: 'Default Non-Serifed Fonts',
     headingStyle: 'merriweather, "times new roman", serif',
     value: 'Arial, sans-serif',
-    label: 'Arial',
   },
+  // roboto
   {
     value: 'roboto, sans-serif',
     label: 'Roboto',
   },
+  // montserrat
   {
     value: 'montserrat, sans-serif',
     label: 'Montserrat',
   },
+  // balsamiq sans
   {
     value: '"balsamiq sans", sans-serif',
     label: 'Balsamiq Sans',
   },
+  // Are you serious
   {
     heading: 'Google Fonts',
     headingStyle: 'sans-serif',
     value: '"Are You Serious", cursive',
     label: 'Are You Serious',
   },
+  // bangers
   {
     value: 'Bangers, cursive',
     label: 'Bangers',
   },
+  // Dancing Script
   {
     value: '"Dancing Script", cursive',
     label: 'Dancing Script',
   },
+  // Great Vibes
   {
     value: '"Great Vibes", cursive',
     label: 'Great Vibes',
   },
+  // Pinyon Script
   {
     value: '"Pinyon Script", cursive',
     label: 'Pinyon Script',
   },
+  // Rye
   {
     value: 'Rye, cursive',
     label: 'Rye',
   },
+  // Sacramento
   {
     value: 'Sacramento, cursive',
     label: 'Sacramento',
   },
+  // Source *
   {
     value: '"Source Code Pro", monospace',
     label: 'Source Code Pro',
@@ -79,6 +93,7 @@ export const Fonts: Font[] = [
     value: '"Source Serif Pro", serif',
     label: 'Source Serif Pro',
   },
+  // Xanh Mono
   {
     value: '"Xanh Mono", monospace',
     label: 'Xanh Mono',
